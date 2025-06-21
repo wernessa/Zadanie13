97cb826c713af468ddb3778f24635c34ffbc1872
Weronika Wąś <weronika.was20@gmail.com>
2025-06-21 14:18:13 +0200

    fixes

--- src/utils/createSupabase.js
+++ src/utils/createSupabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const createSupabase = () => {
-  const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-  const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+  const SUPABASE_URL = 'https://ylolgqlltxgtemyjnogh.supabase.co';
+  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlsb2xncWxsdHhndGVteWpub2doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTczMjMsImV4cCI6MjA2NTc3MzMyM30.2KD8rx2yPdNGSmqB2BLmiYrFb9T5pAPKZ14PGaErGy8';
 
   return createClient(SUPABASE_URL, SUPABASE_KEY);
 };
